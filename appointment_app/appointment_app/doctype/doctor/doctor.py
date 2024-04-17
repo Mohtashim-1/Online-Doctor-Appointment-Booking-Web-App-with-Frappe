0a3efbad990492d5031cefbcd9ce262017559bb7
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-04-17 20:20:26 +0000

    full name validation in doctor.py

--- appointment_app/appointment_app/doctype/doctor/doctor.py
+++ appointment_app/appointment_app/doctype/doctor/doctor.py
@@ -5,4 +5,13 @@
 from frappe.model.document import Document
 
 class Doctor(Document):
-	pass
+	def validate(self):
+		self.set_full_name()
+
+	def set_full_name(self):
+		# also handle if last name is not present
+		self.full_name = (
+			(self.first_name + " " + self.last_name )
+   			if self.last_name else self.first_name
+			)
+
