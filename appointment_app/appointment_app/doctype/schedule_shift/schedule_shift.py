734764fedf24143e13aaaa4458085440100f8b03
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-04-18 11:05:36 +0000

    feat: schedule

--- appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
+++ appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
@@ -1,12 +1,14 @@
 # Copyright (c) 2024, Mohtashim and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 from frappe.utils import format_time
 
 
 class ScheduleShift(Document):
+	frappe.msgprint('Message')
+	
 	def before_save(self):
 		# set the title field to "start_time-end_time"
 		self.title = format_time(self.start_time, "HH:mm") + "-" + format_time(self.end_time, "HH:mm")
