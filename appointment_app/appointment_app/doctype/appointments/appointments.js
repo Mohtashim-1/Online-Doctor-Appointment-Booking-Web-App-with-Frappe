ecdd8e814a0fd68d154eff6a0a5bb349e7ac043c
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-04-18 10:07:51 +0000

    refractor: appointment and schedule_shift setup and filter applied

--- appointment_app/appointment_app/doctype/appointments/appointments.js
+++ appointment_app/appointment_app/doctype/appointments/appointments.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Mohtashim and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Appointments', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Appointments", {
+  refresh(frm) {
+    frm.set_query("shift", (doc) => {
+      return {
+        filters: {
+          clinic: doc.clinic,
+        },
+      };
+    });
+  },
 });
--- appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
+++ appointment_app/appointment_app/doctype/schedule_shift/schedule_shift.py
@@ -3,9 +3,14 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.utils import format_time
+
 
 class ScheduleShift(Document):
 	def before_save(self):
-		# set the title field to start time and end time 
-		self.title = "abcd"
-		# self.title = self.start_time + "-" + self.end_time
+		# set the title field to "start_time-end_time"
+		self.title = format_time(self.start_time, "HH:mm") + "-" + format_time(self.end_time, "HH:mm")
+	
+   			
+			
+		
